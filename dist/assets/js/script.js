021b573909409a6b4ef5201e2be665e0fd8eec2f
nereid <nas.kuleshova@gmail.com>
2018-10-29 17:34:51 +0300

    add header

---
+++ dist/assets/js/script.js
@@ -0,0 +1,8 @@
+"use strict";
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var Shared = function Shared() {
+    _classCallCheck(this, Shared);
+};
+//# sourceMappingURL=script.js.map
